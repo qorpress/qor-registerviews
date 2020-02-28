628aa08ec598a74afa0734fa69aa32bd7fc4ab49
snowly <BrendenAudrina6287@gmail.com>
2020-02-28 13:09:59 +0800

    重构方法

--- registerview.go
+++ registerview.go
@@ -19,60 +19,73 @@ func DetectViewsDir(pkgorg, pkgname, subpath string) string {
 
 	}
 
+	if abspath := isAbs(pkgname, subpath, pkgorg); len(abspath) > 0 {
+		return abspath
+	}
+
+	ppath := filepath.Join(filepath.Join(gopath, "/pkg/mod/"), pkgorg)
+	if _, err := os.Stat(ppath); err == nil {
+		foundp, err := walkPath(ppath, pkgname, subpath)
+		if err == nil {
+			return foundp
+		}
+		color.Red(fmt.Sprintf("os.Stat2 error %v\n", err))
+	} else {
+		color.Red(fmt.Sprintf("os.Stat1 error %v\n", err))
+	}
+
+	return ""
+}
+
+func walkPath(ppath string, pkgname string, subpath string) (string, error) {
+	var foundp string
+	var found bool
+	if err := filepath.Walk(ppath, func(p string, f os.FileInfo, err error) error { // nolint: errcheck, gosec, unparam
+		if found {
+			return nil
+		}
+
+		if foundp = foundPath(p, pkgname, subpath); len(foundp) > 0 {
+			found = true
+		}
+
+		return nil
+
+	}); err != nil {
+		return "", err
+	}
+
+	return foundp, nil
+}
+
+func foundPath(p, pkgname, subpath string) string {
+	if hasPerfix(p, pkgname) {
+		if vp := filepath.Join(p, subpath+"views"); isExistingDir(vp) {
+			return vp
+		}
+	}
+	return ""
+}
+
+func hasPerfix(p string, pkgname string) bool {
+	return strings.HasPrefix(filepath.Base(p), pkgname+"@")
+}
+
+func isAbs(pkgname string, subpath string, pkgorg string) string {
 	if pkgname == "" && subpath == "" {
 		if filepath.IsAbs(pkgorg) {
 			return pkgorg
 		}
+
 		if arp := filepath.Join(utils.AppRoot, "vendor", pkgorg); isExistingDir(arp) {
 			return arp
 		}
+
 		for _, gopath := range utils.GOPATH() {
 			if gp := filepath.Join(gopath, "src", pkgorg); isExistingDir(gp) {
 				return gp
 			}
 		}
-
-		return ""
-	}
-
-	path := filepath.Join(gopath, "/pkg/mod/")
-	ppath := filepath.Join(path, pkgorg)
-	if _, err := os.Stat(ppath); err == nil {
-		var foundp string
-		var found bool
-		if err = filepath.Walk(ppath, func(p string, f os.FileInfo, err error) error { // nolint: errcheck, gosec, unparam
-			if found {
-				return nil
-			}
-
-			if pkgname != "" {
-				if strings.HasPrefix(filepath.Base(p), pkgname+"@") {
-					if vp := filepath.Join(p, "views"); isExistingDir(vp) {
-						foundp = vp
-						found = true
-					} else {
-						return err
-					}
-					if subpath != "" {
-						if strings.HasPrefix(filepath.Base(p), pkgname+"@") {
-							if cvp := filepath.Join(p, subpath+"views"); isExistingDir(cvp) { // auth@
-								foundp = cvp
-								found = true
-							} else {
-								return err
-							}
-						}
-					}
-				}
-			}
-			return nil
-		}); err != nil {
-			color.Red(fmt.Sprintf("os.Stat2 error %v\n", err))
-		}
-
-		return foundp
-	} else {
-		color.Red(fmt.Sprintf("os.Stat1 error %v\n", err))
 	}
 
 	return ""
